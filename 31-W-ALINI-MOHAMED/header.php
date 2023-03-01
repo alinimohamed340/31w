c97e26ffd0ae9c81f2390aee129c9bb73e1aefed
alini mohamed <alinimohamed340@gmail.com>
2023-03-01 11:02:31 -0500

    cours 9 ajout ds mon fichier header.php de get_queried_object pr afficher les cours ds mon aside

--- 31-W-ALINI-MOHAMED/header.php
+++ 31-W-ALINI-MOHAMED/header.php
@@ -25,8 +25,15 @@
     <h3>Menu Secondaire</h3>
     <?php
     $category = get_queried_object();
+    if (isset($category)) 
+    {
+        $lemenu = $category->slug;
+    }else {
+        $lemenu = "note-wp";
+    }
+
     wp_nav_menu(array(
-        "menu" => "cours",
+        "menu" => $lemenu,
         "container" => "nav"
     )); ?>
     </aside>
